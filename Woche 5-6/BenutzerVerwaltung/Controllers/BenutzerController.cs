e7cf6f47f6d75dc32f0cb55feda6e48bf1fa3c90
RomanPeterP <roman@aspnetprofi.com>
2025-04-09 16:12:56 +0200

    upd

---
+++ Woche 5-6/BenutzerVerwaltung/Controllers/BenutzerController.cs
@@ -0,0 +1,44 @@
+﻿using BenutzerVerwaltung.Data;
+using BenutzerVerwaltung.Models;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/benutzer")]
+public class BenutzerController : ControllerBase
+{
+
+    [HttpPost("registrieren")]
+    public IActionResult Registrieren([FromForm] Benutzer benutzer)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        BenutzerData.BenutzerListe.Add(benutzer);
+        return Ok($"Registrierung erfolgreich: {benutzer.Vorname} {benutzer.Nachname}, Alter: {benutzer.Alter}");
+    }
+
+    [HttpPost("registrieren-json")]
+    public IActionResult RegistrierenJson([FromBody] Benutzer benutzer)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        BenutzerData.BenutzerListe.Add(benutzer);
+        return Ok($"Registrierung erfolgreich: {benutzer.Vorname} {benutzer.Nachname}, Alter: {benutzer.Alter}");
+    }
+
+    [HttpGet("{nachname}")]
+    public IActionResult GetBenutzer([FromRoute] string nachname)
+    {
+        var benutzer = BenutzerData.BenutzerListe.FirstOrDefault(b => b.Nachname == nachname);
+        return benutzer != null ? Ok(benutzer) : NotFound("Benutzer nicht gefunden");
+    }
+
+    [HttpGet("liste")]
+    public IActionResult GetAlleBenutzer()
+    {
+        return Ok(BenutzerData.BenutzerListe);
+    }
+}
---
+++ Woche 5-6/BenutzerVerwaltung/Models/Benutzer.cs
@@ -0,0 +1,10 @@
+﻿namespace BenutzerVerwaltung.Models
+{
+    public class Benutzer
+    {
+        public required string Vorname { get; set; }
+        public required string Nachname { get; set; }
+        public int Alter { get; set; }
+    }
+
+}
