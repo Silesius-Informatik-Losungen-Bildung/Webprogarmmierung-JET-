4085f351a5e619eef55dc48bab272e097e021ed4
RomanPeterP <roman@aspnetprofi.com>
2025-04-16 16:06:48 +0200

    upd

---
+++ Woche 7/Buchverwaltung/Controllers/BuchController.cs
@@ -0,0 +1,33 @@
+﻿using Buchverwaltung.Data;
+using Buchverwaltung.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Buchverwaltung.Controllers
+{
+    public class BuchController : Controller
+    {
+        [HttpGet]
+        public IActionResult Erfassen()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Erfassen(Buch buch)
+        {
+            if (ModelState.IsValid)
+            {
+                buch.Id = BuchData.Buecher.Count + 1;
+                BuchData.Buecher.Add(buch);
+                return RedirectToAction("Liste");
+            }
+            return View(buch);
+        }
+
+        [HttpGet]
+        public IActionResult Liste()
+        {
+            return View(BuchData.Buecher);
+        }
+    }
+}
---
+++ Woche 7/Buchverwaltung/Models/Buch.cs
@@ -0,0 +1,10 @@
+﻿namespace Buchverwaltung.Models
+{
+    public class Buch
+    {
+        public int Id { get; set; }
+        public string Titel { get; set; } = null!;
+        public string Autor { get; set; } = null!;
+        public int Erscheinungsjahr { get; set; }
+    }
+}
