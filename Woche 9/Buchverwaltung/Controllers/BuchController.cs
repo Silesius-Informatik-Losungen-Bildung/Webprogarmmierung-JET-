a203b5020c1a902fcf9fedc69d183011d8611578
RomanPeterP <roman@aspnetprofi.com>
2025-05-14 14:47:46 +0200

    upd

---
+++ Woche 9/Buchverwaltung/Controllers/BuchController.cs
@@ -0,0 +1,101 @@
+﻿using Buchverwaltung.Data;
+using Buchverwaltung.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Buchverwaltung.Controllers
+{
+    public class BuchController : Controller
+    {
+        [HttpGet]
+        public IActionResult Erfassen()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Erfassen(Buch buch)
+        {
+            if (ModelState.IsValid)
+            {
+
+                // Anlegen
+                if (!buch.Id.HasValue)
+                {
+                    buch.Id = BuchData.Buecher.Count + 1;
+                    BuchData.Buecher.Add(buch);
+                    return RedirectToAction("Liste");
+                }
+
+
+                // Bearbeiten
+                var vorhandenesBuch = BuchData.Buecher.FirstOrDefault(b => b.Id == buch.Id);
+                if (vorhandenesBuch == null)
+                    return NotFound();
+
+                vorhandenesBuch.Titel = buch.Titel;
+                vorhandenesBuch.ISBN = buch.ISBN;
+                vorhandenesBuch.Autor = buch.Autor;
+                vorhandenesBuch.Erscheinungsjahr = buch.Erscheinungsjahr;
+
+                return RedirectToAction("Liste");
+            }
+            return View(buch);
+        }
+
+        [HttpGet]
+        public IActionResult Liste()
+        {
+            return View(BuchData.Buecher);
+        }
+
+
+        [HttpGet]
+        public IActionResult Bearbeiten(int id)
+        {
+            var buch = BuchData.Buecher.FirstOrDefault(b => b.Id == id);
+            if (buch == null)
+                return NotFound();
+
+            return View("Erfassen", buch);
+        }
+
+        [HttpGet]
+        public IActionResult Loeschen(int id)
+        {
+            var buch = BuchData.Buecher.FirstOrDefault(b => b.Id == id);
+            if (buch == null)
+                return NotFound();
+
+            BuchData.Buecher.Remove(buch);
+            return RedirectToAction("Liste");
+        }
+
+        [HttpGet]
+        public IActionResult AlleLoeschen()
+        {
+            BuchData.Buecher.Clear();
+            return RedirectToAction("Liste");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateSelected([FromRoute] int id, bool isSelected)
+        {
+            var buch = BuchData.Buecher.FirstOrDefault(b => b.Id == id);
+            if (buch == null)
+                return NotFound();
+
+            buch.IsSelected = isSelected;
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult DeleteSelected(ICollection<int> ids)
+        {
+            if (ids != null)
+                BuchData.Buecher.RemoveAll(b => ids.Contains(b.Id.Value));
+
+            return RedirectToAction("Liste");
+        }
+    }
+}
---
+++ Woche 9/Buchverwaltung/Models/Buch.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Buchverwaltung.Models
+{
+    public class Buch
+    {
+        public int? Id { get; set; }
+        public string Titel { get; set; } = null!;
+
+        [MaxLength(6)]
+        public string ISBN { get; set; } = null!;
+        public string Autor { get; set; } = null!;
+
+        [Range(1500, 2025, ErrorMessage = "Bitte geben Sie ein Jahr zwischen 1500 und 2100 ein.")]
+        public int Erscheinungsjahr { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
