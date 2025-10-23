9244aa19851cf0bbaab9e7ee9854a59cc5e5f8e4
RomanPeterP <roman@aspnetprofi.com>
2025-10-23 14:57:12 +0200

    upd

--- Modul 4/Beispiele/PersonenVerwaltung/PersonenVerwaltung/Data/PersonenDbContext.cs
+++ Modul 4/Beispiele/PersonenVerwaltung/PersonenVerwaltung/Data/PersonenDbContext.cs
@@ -6,6 +6,7 @@ namespace PersonenVerwaltung.Data;
 public partial class PersonenDbContext : DbContext
 {
     public virtual DbSet<Person> Personen { get; set; }
+    public virtual DbSet<Standort> Standorte { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
        => optionsBuilder.UseSqlServer("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=PersonenVerwaltung;Data Source=DESKTOP-KCGE85K\\SQLEXPRESS;TrustServerCertificate=true");
