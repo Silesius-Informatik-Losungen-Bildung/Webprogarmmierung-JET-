0bc87c618674edeeee082082f52911f2bf9ae32d
RomanPeterP <roman@aspnetprofi.com>
2026-03-12 08:08:20 +0100

    upd

--- Modul 5/ViewComponents/ViewComponentDemo/ViewComponentDemo/Data/DemoContext.cs
+++ Modul 5/ViewComponents/ViewComponentDemo/ViewComponentDemo/Data/DemoContext.cs
@@ -13,10 +13,10 @@ namespace ViewComponentDemo.Data
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Artikel>().HasData(
-                new Artikel { Id = 1, Titel = "Erster Artikel", ErstelltAm = DateTime.Now.AddDays(-2) },
-                new Artikel { Id = 2, Titel = "Zweiter Artikel", ErstelltAm = DateTime.Now.AddDays(-1) },
-                new Artikel { Id = 3, Titel = "Dritter Artikel", ErstelltAm = DateTime.Now },
-                new Artikel { Id = 4, Titel = "Vierter Artikel", ErstelltAm = DateTime.Now }
+                new Artikel { ArtikelId = 1, Titel = "Erster Artikel", ErstelltAm = DateTime.Now.AddDays(-2) },
+                new Artikel { ArtikelId = 2, Titel = "Zweiter Artikel", ErstelltAm = DateTime.Now.AddDays(-1) },
+                new Artikel { ArtikelId = 3, Titel = "Dritter Artikel", ErstelltAm = DateTime.Now },
+                new Artikel { ArtikelId = 4, Titel = "Vierter Artikel", ErstelltAm = DateTime.Now }
             );
         }
     }
--- Modul 5/ViewComponents/ViewComponentDemo/ViewComponentDemo/Models/Artikel.cs
+++ Modul 5/ViewComponents/ViewComponentDemo/ViewComponentDemo/Models/Artikel.cs
@@ -2,7 +2,7 @@ namespace ViewComponentDemo.Models
 {
     public class Artikel
     {
-        public int Id { get; set; }
+        public int ArtikelId { get; set; }
         public string Titel { get; set; } = null!;
         public DateTime ErstelltAm { get; set; }
     }
